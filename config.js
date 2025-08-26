9913f7bec2c5256420c76c7f33b84e3dfa5c5032
HOOKSTAR <21138869+hookstar1993@users.noreply.github.com>
2025-08-26 15:27:50 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,7 @@
+// IMPORTANT: Do NOT upload this file to GitHub.
+// Add 'config.js' to your .gitignore file.
+
+const config = {
+  apiKey: 'AIzaSyA6vRLnDZ-GLY6NrwMBqpqlGjKrreFJmo8',
+  sheetId: '1OOthoN3XS_GfwYsbQC-WSpJer9FMZ-AYK7E4xuxE4y8'
+};
